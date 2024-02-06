5b504efcc684b2a9e202189a64398c7e8ce740d0
root <root@ince.myguest.virtualbox.org>
2024-02-06 19:35:53 +0100

    modified a few things

--- srcs/requirements/mariaDB/init.sh
+++ srcs/requirements/mariaDB/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "INIT.SH executing... $USER1"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'dgarizad';" > /etc/mysql/tmp
-echo "CREATE USER IF NOT EXISTS '$USER1'@'%' IDENTIFIED BY 'USER1_PASSWORD';" >> /etc/mysql/tmp
-echo "CREATE USER IF NOT EXISTS 'USER2'@'%' IDENTIFIED BY 'USER2_PASSWORD';" >> /etc/mysql/tmp
-
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'MYSQL_ROOT_PASSWORD';" > /etc/mysql/tmp
+echo "CREATE USER IF NOT EXISTS '$USER1'@'%' IDENTIFIED BY '$USER1_PASSWORD';" >> /etc/mysql/tmp
+echo "CREATE USER IF NOT EXISTS '$USER2'@'%' IDENTIFIED BY '$USER2_PASSWORD';" >> /etc/mysql/tmp
+echo "GRANT ALL PRIVILEGES ON *.* TO '$USER1'@'%' ;" >> /etc/mysql/tmp
+echo "GRANT ALL PRIVILEGES ON *.* TO '$USER2'@'%' ;" >> /etc/mysql/tmp
 cat /etc/mysql/tmp /docker-entrypoint-initdb.d/init.sql > /etc/mysql/tmp2
 
 mv /etc/mysql/tmp2 /docker-entrypoint-initdb.d/init.sql
