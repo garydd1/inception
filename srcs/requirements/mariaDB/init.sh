a26836c5e8244d40acd3ad9838e8ff433ca4a117
dgarizad42 <dgarizad42@dgarizad42.dgarizad42>
2024-03-22 14:17:48 -0500

    updated mariadb and wp

--- srcs/requirements/mariaDB/init.sh
+++ srcs/requirements/mariaDB/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'MYSQL_ROOT_PASSWORD';" > /etc/mysql/tmp
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" > /etc/mysql/tmp
 echo "CREATE USER IF NOT EXISTS '$USER1'@'%' IDENTIFIED BY '$USER1_PASSWORD';" >> /etc/mysql/tmp
 echo "CREATE USER IF NOT EXISTS '$USER2'@'%' IDENTIFIED BY '$USER2_PASSWORD';" >> /etc/mysql/tmp
 echo "GRANT ALL PRIVILEGES ON *.* TO '$USER1'@'%' ;" >> /etc/mysql/tmp
@@ -8,5 +8,6 @@ echo "GRANT ALL PRIVILEGES ON *.* TO '$USER2'@'%' ;" >> /etc/mysql/tmp
 cat /etc/mysql/tmp /docker-entrypoint-initdb.d/init.sql > /etc/mysql/tmp2
 
 mv /etc/mysql/tmp2 /docker-entrypoint-initdb.d/init.sql
+sed -i 's/^bind-address\s*=.*$/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 exec mysqld --init-file=/docker-entrypoint-initdb.d/init.sql
---
+++ srcs/requirements/wordpress/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /var/www/html
+
+wp core download --allow-root --version=5.8.1 --locale=en_US
+wp config create --allow-root --dbname=${DB_NAME} --dbuser=${USER1} --dbpass=${WP_PASSWORD} --dbhost=${DB_HOST}
+wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+
+wp user create "${WP_USER}" "dgarizado2@gmail.com" --user_pass="${WP_PASSWORD}" --role=author --allow-root
+exec /usr/sbin/php-fpm7.4 -F -R
