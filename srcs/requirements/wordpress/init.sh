b4da0734e398f0b9a20827f43f017d06b263fff7
dgarizad42 <dgarizad42@dgarizad42.dgarizad42>
2024-03-29 12:34:11 -0500

    one step closer. before reseting  alldocker

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -7,4 +7,5 @@ wp config create --allow-root --dbname=${DB_NAME} --dbuser=${USER1} --dbpass=${W
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 
 wp user create "${WP_USER}" "dgarizado2@gmail.com" --user_pass="${WP_PASSWORD}" --role=author --allow-root
+sed -i 's/^listen =.*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 exec /usr/sbin/php-fpm7.4 -F -R
